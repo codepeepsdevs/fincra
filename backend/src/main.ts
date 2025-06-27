76cb4d8e75f74e36b4093ce1cdf119c32abcb072
abraham <abrahamosazee3@gmail.com>
2025-06-27 15:26:37 +0100

    add frontend url to backend cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://frontend-production-56bd.up.railway.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
     credentials: true,
